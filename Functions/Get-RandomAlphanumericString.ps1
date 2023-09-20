cdfd6b60587d521e7dfb55d780e28111db173182
Jon LaBelle <contact@jonlabelle.com>
2023-09-20 18:21:43 -0500

    Generate strings larger than just 62 chars

--- Functions/Get-RandomAlphanumericString.ps1
+++ Functions/Get-RandomAlphanumericString.ps1
@@ -7,5 +7,9 @@ function Get-RandomAlphaNumericString
     [OutputType([String])]
     param ([int] $Length = 12)
 
-    Write-Output (-join ((0x30..0x39) + ( 0x41..0x5A) + ( 0x61..0x7A) | Get-Random -Count $Length | ForEach-Object { [char]$_ }))
+    # Write-Output (-join ( (0x30..0x39) + ( 0x41..0x5A) + ( 0x61..0x7A) | Get-Random -Count $Length | ForEach-Object { [char]$_ }))
+
+    # Adapted from: Using ForEach with a nested call to Get-Random on the input ranges is another way to avoid repeats:
+    # https://gist.github.com/gregjhogan/2350eb60d02aa759c9d269c3fc6265b1?permalink_comment_id=4053128#gistcomment-4053128
+    -join (1..$Length | ForEach-Object {[char]((0x30..0x39) + ( 0x41..0x5A) + ( 0x61..0x7A) | Get-Random)})
 }
