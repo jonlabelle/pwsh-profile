bc45a3da2ec3e43e2d7f06fc188084c63e018ac3
Jon LaBelle <contact@jonlabelle.com>
2023-09-20 17:58:12 -0500

    Proper casing for function name, param

--- Functions/Get-RandomAlphanumericString.ps1
+++ Functions/Get-RandomAlphanumericString.ps1
@@ -1,15 +1,11 @@
 #
 # Generate a random Alphanumeric string
 # https://jonlabelle.com/snippets/view/powershell/generate-random-alphanumeric-string-in-powershell
-function Get-RandomAlphanumericString
+function Get-RandomAlphaNumericString
 {
     [CmdletBinding()]
-    Param ([int] $length = 12)
+    [OutputType([String])]
+    param ([int] $Length = 12)
 
-    Begin {}
-
-    Process
-    {
-        Write-Output ( -join ((0x30..0x39) + ( 0x41..0x5A) + ( 0x61..0x7A) | Get-Random -Count $length | ForEach-Object { [char]$_ }))
-    }
+    Write-Output (-join ((0x30..0x39) + ( 0x41..0x5A) + ( 0x61..0x7A) | Get-Random -Count $Length | ForEach-Object { [char]$_ }))
 }
