fb4d6c2b4005978611df78217e7257fa5210c599
Jon LaBelle <contact@jonlabelle.com>
2023-11-04 12:19:40 -0500

    Add Test-ADCredential function

---
+++ Functions/Test-ADCredential.ps1
@@ -0,0 +1,43 @@
+function Test-ADCredential
+{
+    <#
+    .SYNOPSIS
+        Test the username and password of Active Directory credentials.
+
+    .LINK
+        https://jonlabelle.com/snippets/view/powershell/test-windows-credential
+    #>
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingWriteHost', '')]
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [PSCredential]
+        $Credential
+    )
+
+    begin
+    {
+        Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+    }
+
+    process
+    {
+        $user = $Credential.username
+        $password = $Credential.GetNetworkCredential().password
+
+        $domain = 'LDAP://' + ([ADSI]'').distinguishedName
+        $de = New-Object System.DirectoryServices.DirectoryEntry($domain, $user, $password)
+
+        if ($null -eq $de.name)
+        {
+            Write-Error "Authentication failed for '$user'"
+        }
+        else
+        {
+            Write-Host "Successfully authenticated with '$user'" -ForegroundColor Green
+        }
+
+        $de.Dispose()
+    }
+}
