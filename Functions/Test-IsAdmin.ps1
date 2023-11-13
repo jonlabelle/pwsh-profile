41225d429bb5efb5dc4ff719857b5492dfdbe7b1
Jon LaBelle <contact@jonlabelle.com>
2023-11-13 16:09:13 -0600

    Add Test-IsAdmin

---
+++ Functions/Test-IsAdmin.ps1
@@ -0,0 +1,14 @@
+function Test-IsAdmin
+{
+    <#
+    .SYNOPSIS
+        Determines if the console is elevated.
+
+    .LINK
+        https://jonlabelle.com/snippets/view/powershell/check-if-the-current-user-is-an-administrator
+    #>
+    $identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object System.Security.Principal.WindowsPrincipal($identity)
+
+    return $principal.IsInRole( [System.Security.Principal.WindowsBuiltInRole]::Administrator)
+}
