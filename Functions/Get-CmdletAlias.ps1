161dc45b2218077021365755872c7af3b9c76058
Jon LaBelle <contact@jonlabelle.com>
2025-08-24 19:23:23 -0400

    feat(Get-CmdletAlias): refactor function to improve parameter handling and add verbose logging

--- Functions/Get-CmdletAlias.ps1
+++ Functions/Get-CmdletAlias.ps1
@@ -1,4 +1,4 @@
-function Get-CmdletAlias ($CmdletName)
+function Get-CmdletAlias
 {
     <#
     .SYNOPSIS
@@ -7,15 +7,19 @@ function Get-CmdletAlias ($CmdletName)
     .DESCRIPTION
         This function retrieves and displays all defined aliases that reference a specific cmdlet.
         It helps users discover alternative shorthand ways to call frequently used cmdlets.
+        Supports wildcard patterns for flexible cmdlet name matching.
+
+        Compatible with PowerShell Desktop 5.1+ on Windows, macOS, and Linux.
 
     .PARAMETER CmdletName
         The name of the cmdlet to find aliases for. Can be a partial name (wildcards are supported).
+        This parameter is mandatory and supports pipeline input.
 
     .EXAMPLE
-        PS> Get-CmdletAlias Get-ChildItem
+        PS> Get-CmdletAlias -CmdletName Get-ChildItem
 
-        Definition  Name
-        ----------  ----
+        Definition    Name
+        ----------    ----
         Get-ChildItem dir
         Get-ChildItem gci
         Get-ChildItem ls
@@ -23,28 +27,84 @@ function Get-CmdletAlias ($CmdletName)
         Lists all aliases defined for the Get-ChildItem cmdlet.
 
     .EXAMPLE
-        PS> Get-CmdletAlias Select
+        PS> Get-CmdletAlias -CmdletName Select*
 
-        Definition       Name
-        ----------       ----
-        Select-Object    select
-        Select-String    sls
+        Definition    Name
+        ----------    ----
+        Select-Object select
+        Select-String sls
 
-        Lists all aliases for cmdlets that contain "Select" in their name.
+        Lists all aliases for cmdlets that start with "Select".
+
+    .EXAMPLE
+        PS> 'Get-Process', 'Get-Service' | Get-CmdletAlias
+
+        Definition  Name
+        ----------  ----
+        Get-Process gps
+        Get-Process ps
+        Get-Service gsv
+
+        Gets aliases for multiple cmdlets using pipeline input.
 
     .OUTPUTS
-        System.Management.Automation.PSCustomObject
+        System.Object
         A formatted table showing the cmdlet definition and its corresponding aliases.
 
     .NOTES
         This function is commonly added to PowerShell profiles for quick alias reference.
-        Source: Microsoft PowerShell documentation
+        Compatible with PowerShell Desktop 5.1+ and PowerShell Core 6+.
+        Works on Windows, macOS, and Linux.
 
     .LINK
         https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles
+
+    .LINK
+        https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.2#add-a-function-that-lists-the-aliases-for-any-cmdlet
     #>
-    # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.2#add-a-function-that-lists-the-aliases-for-any-cmdlet
-    Get-Alias |
-    Where-Object -FilterScript {$_.Definition -like "$CmdletName"} |
-    Format-Table -Property Definition, Name -AutoSize
+    [CmdletBinding()]
+    [OutputType([System.Object])]
+    param
+    (
+        [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $CmdletName
+    )
+
+    begin
+    {
+        Write-Verbose 'Starting cmdlet alias lookup'
+    }
+
+    process
+    {
+        Write-Verbose "Looking up aliases for cmdlet: '$CmdletName'"
+
+        try
+        {
+            $aliases = Get-Alias | Where-Object -FilterScript { $_.Definition -like "$CmdletName" }
+
+            if ($aliases)
+            {
+                Write-Verbose "Found $($aliases.Count) alias(es) for '$CmdletName'"
+                $aliases | Format-Table -Property Definition, Name -AutoSize
+            }
+            else
+            {
+                Write-Verbose "No aliases found for cmdlet: '$CmdletName'"
+                Write-Warning "No aliases found for cmdlet: '$CmdletName'"
+            }
+        }
+        catch
+        {
+            Write-Verbose "Error occurred while looking up aliases: $($_.Exception.Message)"
+            throw $_
+        }
+    }
+
+    end
+    {
+        Write-Verbose 'Cmdlet alias lookup completed'
+    }
 }
